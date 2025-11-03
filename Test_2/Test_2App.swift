8da23ab8c4f364f02d2214b0fc5201eca75f5dea
Hsuanting <colbynn99@gmail.com>
2025-11-03 09:23:11 +0800

    Initial Commit

---
+++ Test_2/Test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_2App.swift
+//  Test_2
+//
+//  Created by Hsuanting on 2025/11/3.
+//
+
+import SwiftUI
+
+@main
+struct Test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
